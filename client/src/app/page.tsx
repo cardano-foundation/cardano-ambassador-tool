7df595a5e7254adfad723848e4967b5a0adb8cc9
sawe-daisy <deesawe16@gmail.com>
2025-07-02 23:21:45 +0300

    bootstrap ambassadors nextjs app

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 const n: number = "this is a string";
 const n2: number = "this is a string";
+const n3: number = "this is a string";
 
 export default function Home() {
   return (
