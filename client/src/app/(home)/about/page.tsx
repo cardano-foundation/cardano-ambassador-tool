a37a9872b332f84070720b600aab77e9dd1037b2
sawe-daisy <deesawe16@gmail.com>
2025-08-27 13:32:46 +0300

    feat: single ambassadors page

--- client/src/app/(home)/about/page.tsx
+++ client/src/app/(home)/about/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { TableTestPage } from '../_components/TableTestPage';
+import GithubIcon from '@/components/atoms/GithubIcon';
+import LinkedInIcon from '@/components/atoms/LinkedinIcon';
+import XIcon from '@/components/atoms/XIcon';
 
 export default function Home() {
   return (
     <div>
       <TableTestPage />
+      <div className="flex gap-4 p-4">
+        <GithubIcon size={24} className="rounded hover:text-gray-600 cursor-pointer" />
+        <LinkedInIcon size={24} color="#0077B5" className="hover:opacity-80 cursor-pointer rounded" />
+        <XIcon size={24} className="hover:text-gray-600 cursor-pointer" />
+      </div>
     </div>
   );
 }
